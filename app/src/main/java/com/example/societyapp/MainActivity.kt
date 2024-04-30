b0afc67fbf1620cab41fc103305d06f20efe7941
SHREE <mansikothari46@gmail.com>
2024-04-29 23:31:27 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/societyapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.societyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.societyapp.ui.theme.SocietyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SocietyAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   SocietyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SocietyApp() {
+
+}
